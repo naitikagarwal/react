dc6379d9c68b798175d7c4230b407b72d52308dd
Naitik Agarwal <naitikagar12@gmail.com>
2024-10-02 16:23:10 +0530

    final

--- props-basic/src/ItemDescription.js
+++ props-basic/src/ItemDescription.js
@@ -1,13 +1,26 @@
-import React from 'react'
+// import React from 'react'
 
-function ItemDescription({name, description}) {
-  return (
-    <div>
-      <p>{name}</p>
-      <p><i>{description}</i></p>
+// function ItemDescription({name, description}) {
+//   return (
+//     <div>
+//       <p>{name}</p>
+//       <p><i>{description}</i></p>
 
-    </div>
-  )
+//     </div>
+//   )
+// }
+
+// export default ItemDescription
+
+import React, { Component } from 'react'
+
+export default class ItemDescription extends Component {
+  render() {
+    return (
+      <div>
+        <p> <b>{this.props.name}</b></p>
+        <p>{this.props.description}</p>
+      </div>
+    )
+  }
 }
-
-export default ItemDescription
